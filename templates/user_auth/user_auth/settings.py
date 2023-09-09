85330ae2c7c4c324b5126900ed5b3add28b3afe7
x-candy6 <contact@c-andrew.com>
2023-09-09 15:32:24 -0700

    Merged changes from JobTracker

--- templates/user_auth/user_auth/settings.py
+++ templates/user_auth/user_auth/settings.py
@@ -38,6 +38,8 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "User.apps.UserConfig",
+    "ListingFeed.apps.ListingfeedConfig",
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -47,6 +49,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -55,6 +58,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = f"{PROJECT_NAME}.urls"
 
